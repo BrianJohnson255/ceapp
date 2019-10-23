252fd0032a6b465423e76cba3043cacf3410e7c1
Addison Bean <addisonbean@gmail.com>
2019-10-23 10:42:19 -0500

    Add a home page

--- client/ceapp/App.js
+++ client/ceapp/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Home from './app/pages/Home';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+	<Home />
+    // <View style={styles.container}>
+      // <Text>Open up App.js to start working on your app!</Text>
+    // </View>
   );
 }
 
