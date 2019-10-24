73b72ea2e688eb37eba6ab400cef70ed9a452a10
Addison Bean <addisonbean@gmail.com>
2019-10-24 14:08:26 -0500

    Create User model and controller

---
+++ server/ceapp/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+	has_secure_password
+
+	# TODO: email regex or something?
+	validates :email, presence: true, uniqueness: true
+
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+end
