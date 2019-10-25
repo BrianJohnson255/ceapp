c2611e7008e828df5f863616186beab711670f7a
LuxMage <luxionaeronia@gmail.com>
2019-10-25 12:14:14 -0500

    Updated home page template with text

--- client/ceapp/app/pages/Home.js
+++ client/ceapp/app/pages/Home.js
@@ -5,11 +5,25 @@ export default class Home extends Component {
 	render() {
 		return (
 			<View style={{ flex: 1, flexDirection: 'column', alignItems: 'stretch', justifyContent: 'center',}}>
-				<View style={{ width: '100%', height: '20%', backgroundColor: 'skyblue'}} />
-				<View style={{ width: '100%', height: '20%', backgroundColor: 'indianred'}} />
-				<View style={{ width: '100%', height: '20%', backgroundColor: 'darkseagreen'}} />
-				<View style={{ width: '100%', height: '20%', backgroundColor: 'mediumpurple'}} />
-				<View style={{ width: '100%', height: '20%', backgroundColor: 'gold'}} />
+				<View style={{ width: '100%', height: '20%', backgroundColor: 'skyblue', justifyContent: 'center', alignItems: 'center' }}>
+					<Text style={{ color: 'white', fontSize: 30 }}>Calendar</Text>
+				</View>
+				
+				<View style={{ width: '100%', height: '20%', backgroundColor: 'indianred', justifyContent: 'center', alignItems: 'center' }}>
+					<Text style={{ color: 'white', fontSize: 30 }}>Active Productions</Text>
+				</View>
+				
+				<View style={{ width: '100%', height: '20%', backgroundColor: 'darkseagreen', justifyContent: 'center', alignItems: 'center' }}>
+					<Text style={{ color: 'white', fontSize: 30 }}>Find Directors</Text>
+				</View>
+				
+				<View style={{ width: '100%', height: '20%', backgroundColor: 'mediumpurple', justifyContent: 'center', alignItems: 'center' }}>
+					<Text style={{ color: 'white', fontSize: 30 }}>Find Crew</Text>
+				</View>
+				
+				<View style={{ width: '100%', height: '20%', backgroundColor: 'gold', justifyContent: 'center', alignItems: 'center' }}>
+					<Text style={{ color: 'white', fontSize: 30 }}>Create Production</Text>
+				</View>
 			</View>
 		);
 	}
