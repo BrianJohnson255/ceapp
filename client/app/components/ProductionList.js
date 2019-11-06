050d4f6ff583a2224409a231189a1ad2e33e0055
Addison Bean <addisonbean@gmail.com>
2019-11-05 20:37:56 -0600

    Fix production list

--- client/app/components/ProductionList.js
+++ client/app/components/ProductionList.js
@@ -1,39 +1,20 @@
-
 import React from 'react';
 import { View, FlatList, StyleSheet, Text } from 'react-native';
 import ProductionListRow from './ProductionListRow.js';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-});
-
-/*
 const ProductionList = ({ itemList }) => (
-    <View style={styles.container}>
-        <FlatList
-                data={itemList}
-                renderItem={({ item }) => <ProductionListRow
-                    title={item.title}
-                    description={item.description}
-                    image_url={item.image_url}
-                />}
-                keyExtractor={item => item.id}
-            />
+	<View>
+		<FlatList
+				data={itemList}
+				renderItem={({ item }) => <ProductionListRow
+					title={item.title}
+					description={item.description}
+					imageUrl={item.imageUrl}
+				/>}
+				keyExtractor={item => item.id}
+			/>
 
-    </View>
-);*/
-
-const ProductionList = ({ itemList }) => (
-    <View style={styles.container}>
-        <FlatList
-            //data={[{"title": "cats", "key": "2"}]}
-            renderItem={({ item }) => <Text>Hello</Text>}
-            keyExtractor={item => item.key}
-        />
-
-    </View>
+	</View>
 );
 
 export default ProductionList;
--- client/app/pages/Production.js
+++ client/app/pages/Production.js
@@ -6,19 +6,18 @@ import ProductionListRow from "../components/ProductionListRow.js";
 export default class Home extends Component {
 	getData(){
 		return [
-				{
-					title: "Dogs",
-					description: "about dogs",
-					image_url: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTQ3NjM5ODIyNjU0MTIxMDM0/snoop_dogg_photo_by_estevan_oriol_archive_photos_getty_455616412.jpg",
-					id: "1"
-				}
-		]
+			{
+				title: "Dogs",
+				description: "about dogs",
+				imageUrl: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTQ3NjM5ODIyNjU0MTIxMDM0/snoop_dogg_photo_by_estevan_oriol_archive_photos_getty_455616412.jpg",
+				id: "1",
+			},
+		];
 	}
 	render() {
 		const { navigate } = this.props.navigation;
 		
 		return (
-
 			<View style={styles.item}>
 				<ProductionList
           			itemList={this.getData()}
