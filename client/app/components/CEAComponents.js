6f6a5d8e05d98da9b400b0a29fa2680730ca8215
LuxMage <luxionaeronia@gmail.com>
2019-11-08 15:59:47 -0600

    Changed home page so that it uses React Native Paper elements

---
+++ client/app/components/CEAComponents.js
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+import * as React from 'react';
+import { Button } from 'react-native-paper';
+
+const CEAButton = () => (
+	<Button mode="contained"></Button>
+);
+
+export default CEAButton;
