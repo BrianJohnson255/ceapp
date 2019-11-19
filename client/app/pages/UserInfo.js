c07cfc9ee0f6fd7c7166ee6481a53a14dd342d53
Jonny <jonnyhughes312@gmail.com>
2019-11-19 15:28:17 -0600

    Added Log Out button to the UserInfo.js Page which both sends the user back to StartUp.js as well as call the logout function

--- client/app/pages/UserInfo.js
+++ client/app/pages/UserInfo.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, ScrollView, Keyboard, TouchableWithoutFeedback, Image } from 'react-native';
 import { Appbar, Button, TextInput } from 'react-native-paper';
+import { switchScreen } from '../util';
+import { logout } from '../api/user';
 
 export default class UserInfo extends Component {
 	render() {
 		const { goBack } = this.props.navigation;
+		const { navigate } = this.props.navigation;
 		
 		return (
 			<View>
@@ -13,7 +16,23 @@ export default class UserInfo extends Component {
 					<Appbar.Content title="Profile" />
 				</Appbar.Header>
 				<Image style={{}}source={require('./CEALogo.png')}/>
+				<Text>{`\n`}</Text>
+				<View style={{ ...styles.item,  backgroundColor: 'white' }}>
+					<Button style={{ ...styles.button}} mode="contained" color="#260859" onPress={() => {logout(); navigate('StartUp')}}>Log Out</Button>
+				</View>
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	button: {
+		width: '50%',
+		
+			
+	},
+	item: {
+		width: '100%',
+		alignItems: 'center',
+	},
+});
