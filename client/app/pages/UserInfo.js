c224f5dcd901b0bf273738fe438bdbb48f7d0720
Paul Swindell <swindellpt@mail.lipscomb.edu>
2019-11-19 15:37:04 -0600

    Updated User Info

--- client/app/pages/UserInfo.js
+++ client/app/pages/UserInfo.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, ScrollView, Keyboard, TouchableWithoutFeedback, Image } from 'react-native';
-import { Appbar, Button, TextInput } from 'react-native-paper';
+import { Appbar, Button, TextInput, Paragraph } from 'react-native-paper';
 
 export default class UserInfo extends Component {
 	render() {
@@ -8,12 +8,40 @@ export default class UserInfo extends Component {
 		
 		return (
 			<View>
-				<Appbar style={{top: 0, height: 70}}>
+				<View style={{height: '100%'}}>
+				
+				<Appbar.Header>
 					<Appbar.BackAction onPress={() => goBack()} />
 					<Appbar.Content title="Profile" />
-				</Appbar>
-				<Image style={{}}source={require('./CEALogo.png')}/>
+				</Appbar.Header>
+				<ScrollView contentInset={{bottom: 150}}>
+			
+				<Image style={{...styles.item}}source={require('./CEALogo.png')}/>
+				
+				<View >
+				<Paragraph style ={{...styles.paragraph}}>Name: </Paragraph>
+				<Paragraph style ={{...styles.paragraph}}>Email: </Paragraph>
+				<Paragraph style ={{...styles.paragraph}}>Phone Number: </Paragraph>
+				<Paragraph style ={{...styles.paragraph}}>Productions: </Paragraph>
+				<Paragraph style ={{...styles.paragraph}}>Skills: </Paragraph>
+				</View>
+				</ScrollView>
+				</View>
 			</View>
 		);
 	}
 }
+const styles = StyleSheet.create({
+	item: {
+		width: '100%',
+		alignItems: 'center',
+		 
+	},
+
+	paragraph: {
+		fontSize: 25,
+		color: '#260859',
+		justifyContent: 'left',
+		height: '10%',
+	}
+});
